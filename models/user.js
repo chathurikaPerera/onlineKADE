69cb043dbd24b17e2ac969c6da1ca9dbf67ba0df
chathurikaPerera <pererachathurika96@gmail.com>
2021-03-17 00:05:28 +0530

    2021.3.16

--- models/user.js
+++ models/user.js
@@ -1,9 +1,58 @@
 const mongoose = require('mongoose');
 
 //create schema
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    passwordHash: {
+        type:String,
+        required:true,
+    },
+    phone: {
+        type:String,
+        required:true,
+    },
+    isAdmin: {
+        type:Boolean,
+        default: false,
+    },
+    street: {
+        type:String,
+        default:'',
+    },
+    apartment: {
+        type:String,
+        default:''
+    },
+    zip: {
+        type:String,
+        default:'',
+    },
+    city: {
+        type:String,
+        default:'',
+    },
+    country: {
+        type:String,
+        default:'',
+    }
     
-})
+});
+userSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+    
+});
+
+userSchema.set('toJSON',{
+    virtual: true,
+});
 
 //create model
 exports.User = mongoose.model('User', userSchema);
+export.userSchema = userSchema;
