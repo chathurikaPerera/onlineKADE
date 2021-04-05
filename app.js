80d8e1b779542acdc294e8bf0579a958314435a5
chathurikaPerera <pererachathurika96@gmail.com>
2021-04-05 20:32:23 +0530

    2020/4/5

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 
 require('dotenv/config');
 const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
 app.options('*', cors());
@@ -16,7 +17,9 @@ const api = process.env.API_URL;
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
-app.use(authJwt);
+app.use(authJwt());
+app.use(errorHandler);
+
 
 
 //routes
--- routes/users.js
+++ routes/users.js
@@ -60,7 +60,7 @@ router.post('/login', async(req,res) => {
                 userId: user.id 
             },
             secret,
-            {expiresIn: '1d'}
+            //{expiresIn: '1d'}
     )
 
         res.status(200).send({user: user.email, token: token});
@@ -70,6 +70,28 @@ router.post('/login', async(req,res) => {
     }
 
     
+})
+
+router.post('/register', async(req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+
 })
 
 module.exports = router;
