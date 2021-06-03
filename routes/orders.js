df913b2e06bd1ae36890f6f8436d2c9a30c452ec
chathurikaPerera <pererachathurika96@gmail.com>
2021-06-03 22:51:56 +0530

    2021-06-03

--- routes/orders.js
+++ routes/orders.js
@@ -16,9 +16,7 @@ router.get(`/`, async (req, res)=>{
 router.get(`/:id`, async (req, res)=>{
     const order = await Order.findById(req.params.id)
     .populate('user','name')
-    .populate({
-        path:'orderItems', populate: {
-            path: 'product', populate: 'category'}
+    .populate({ path:'orderItems', populate: { path: 'product', populate: 'category'}
         });
 
     if(!order){
@@ -38,8 +36,15 @@ router.post('/', async(req, res)=>{
         return newOrderItem._id; 
     }))
      const orderItemsIdsResolved = await orderItemsIds;
-
     
+    const totalPrices = await Promise.all(orderItemsIdsResolved.map(async(orderItemId) =>{
+        const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+        return totalPrice;
+    }))
+    console.log(totalPrices);
+    const totalPrice = totalPrices.reduce((a,b) => a+b, 0);
+
     let order = new Order({
         orderItems:orderItemsIdsResolved ,
         shippingAddress1: req.body.shippingAddress1,
@@ -49,7 +54,7 @@ router.post('/', async(req, res)=>{
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
+        totalPrice: totalPrice,
         user: req.body.user,
     }) 
     order = await order.save();
@@ -78,9 +83,12 @@ router.post('/', async(req, res)=>{
 
  //delete the order
  router.delete('/:id', async(req, res) =>{
-     Order.findByIdAndRemove(req.params.id).then(order => {
+     Order.findByIdAndRemove(req.params.id).then(async order => {
          if(order)
         {
+            await order.orderItems.map(async order => {
+                await OrderItem.findByIdAndRemove(orderItem) // can used errors in here
+            })
             return res.status(200).json({success:true, message:"order delete successfully"});
         }
         else{
